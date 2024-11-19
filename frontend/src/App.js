54e4749b92f0f9886962e9681b0de7aed377c2bf
MalekSghaier <melak.seghaier@gmail.com>
2024-11-19 01:35:47 +0100

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,34 @@
+
+import './App.css';
+import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Notfound from './pages/NotFound';
+import { CssBaseline,ThemeProvider } from '@mui/material';
+import { theme } from './theme';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import LogIn from './pages/LogIn';
+
+const App = () => {
+  return (
+    <>
+        <ToastContainer/>
+        <ThemeProvider theme={theme}>
+          <CssBaseline/>
+        < BrowserRouter>
+            <Routes>
+               <Route path='/' element={<Home/>}/>
+               <Route path='/search/location/:location' element={<Home />} />
+               <Route path='/search/:keyword' element={<Home />} />
+               <Route path='/login' element={<LogIn />} />
+               <Route path='*' element={<Notfound/>}/>
+            </Routes>
+          
+        </BrowserRouter>
+        </ThemeProvider>
+    </>
+  )
+}
+
+export default App
